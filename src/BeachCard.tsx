e7eabb1c4ebbdf29677d480d91b21f803ceecb85
oi18aln <btu2Tvbg>
2023-05-04 15:09:26 +0200

    Added initial routing

---
+++ src/BeachCard.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+type Beach = {
+    name: string;
+};
+
+type Props = {
+    beach: Beach;
+};
+
+const BeachCard = (props: Props) => {
+    return <div>BeachCard</div>;
+};
+
+export default BeachCard;
---
+++ src/BeachList.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+type Props = {
+    adress?: string;
+};
+
+const BeachList = (props: Props) => {
+    // hämta data här
+    return <div>BeachList</div>;
+};
+
+export default BeachList;
