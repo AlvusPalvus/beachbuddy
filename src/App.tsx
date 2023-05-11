4c42724af5c7c3e7ed9915c8bdc285c3a1aa5918
oi18aln <btu2Tvbg>
2023-05-11 20:08:48 +0200

    Added a map loader to start page
    Maybe the user can choose its position from the map?

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
 import { Link } from "react-router-dom";
+import MapLoader from "./MapLoader";
+import AddressForm from "./AddressForm";
+import { useState } from "react";
+import { LatLngLiteral } from "./types/googleTypes";
 
 function App() {
+    const [userPosition, setUserPosition] = useState<LatLngLiteral>({
+        lat: 63.825,
+        lng: 20.263,
+    });
+
     return (
         <div className="App">
             <p>Start! Hitta din badplats</p>
+
             <Link to={"/badplatser"}> Hitta badplatser </Link>
+            {/* <AddressForm /> */}
+            <MapLoader userPosition={userPosition} />
         </div>
     );
 }
---
+++ src/MapLoader.tsx
@@ -0,0 +1,41 @@
+import React, { useMemo } from "react";
+import { GoogleMap, Marker, useJsApiLoader } from "@react-google-maps/api";
+import { LatLngLiteral } from "./types/googleTypes";
+
+type Props = {
+    userPosition: LatLngLiteral;
+};
+
+const MapLoader = ({ userPosition }: Props) => {
+    const { isLoaded } = useJsApiLoader({
+        googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY as string,
+    });
+    let center = useMemo(
+        () => ({ lat: userPosition.lat, lng: userPosition.lng }),
+        [userPosition]
+    );
+
+    if (process.env.REACT_APP_GOOGLE_MAPS_API_KEY === undefined) {
+        console.error("You need the access key");
+        return <div>mapLoader failed</div>;
+    }
+
+    if (!isLoaded) {
+        return <div>Loading...</div>;
+    } else return Map(center);
+};
+
+function Map(center: LatLngLiteral) {
+    console.log(center);
+    return (
+        <GoogleMap
+            mapContainerClassName="mapContainer"
+            zoom={10}
+            center={center}
+        >
+            <Marker position={center}></Marker>
+        </GoogleMap>
+    );
+}
+
+export default MapLoader;
