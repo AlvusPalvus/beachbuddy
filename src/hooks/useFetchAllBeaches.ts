b8a737b23131e9e54e7ae321bc30834c6c23bd18
oi18aln <btu2Tvbg>
2023-05-19 18:26:15 +0200

    Refactoring data fetching
    Started on error handeling

---
+++ src/hooks/useFetchAllBeaches.ts
@@ -0,0 +1,50 @@
+import React, { useEffect, useState } from "react";
+import { Beach, UserOptions } from "../types/beachTypes";
+import { getBeachData } from "../functions/getBeachData";
+import { addWeatherData } from "../functions/addWeatherData";
+import addDestinationData from "../functions/addDestinationData";
+import { LatLngLiteral, TravelMode } from "../types/googleTypes";
+
+type Props = {
+    setIsPending: Function;
+    setError: Function;
+    userOptions: UserOptions;
+};
+
+const openDataUrl: string =
+    "https://opendata.umea.se/api/records/1.0/search/?dataset=badplatser&q=&rows=33&facet=namn&facet=omrade&facet=handik_anp";
+
+function useFetchAllBeaches({ setIsPending, setError, userOptions }: Props) {
+    const [beaches, setBeaches] = useState<Beach[]>();
+    const [openDataError, setOpenDataError] = useState("");
+    const [smhiError, setSmhiError] = useState("");
+    const [mapsError, setMapsError] = useState("");
+
+    useEffect(() => {
+        const fetchAllData = async () => {
+            let beachData: Beach[] = await getBeachData(
+                openDataUrl,
+                setOpenDataError
+            );
+
+            if (beachData.length > 0) {
+                await addWeatherData(beachData, setSmhiError);
+
+                await addDestinationData({
+                    beachList: beachData,
+                    userOptions: userOptions,
+                    setMapsError,
+                });
+            }
+            setBeaches(beachData);
+            setError(openDataError + smhiError + +mapsError);
+            setIsPending(false);
+        };
+
+        setIsPending(true);
+        fetchAllData();
+    }, []);
+    return beaches;
+}
+
+export default useFetchAllBeaches;
