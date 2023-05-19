0abd2bca9ea62fc70406c3cb377fc89c428648e8
ens19awt <120040065+agnes4@users.noreply.github.com>
2023-05-19 10:22:16 +0200

    Added icons to Header component

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,3 +1,5 @@
+import { TiWaves } from "react-icons/ti";
+
 export const Header = () => {
     const headerStyles = "header flex items-space-between bg-green-100 py-7";
     const logoStyles = "logo flex flex-col items-center text-lbold leading-7";
@@ -8,7 +10,9 @@ export const Header = () => {
                 <span>BEACH</span>
                 <span>BUDDY</span>
             </div>
-            <div className="header-icons"></div>
+            <div className="header-icons flex flex-row items-center">
+                <TiWaves size={50} />
+            </div>
         </div>
     );
 };
