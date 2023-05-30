30f1de10f44d79db16799e3ce9d504d909d96fc2
ens19awt <120040065+agnes4@users.noreply.github.com>
2023-05-30 08:26:45 +0200

    Formatted BeachDetails component

--- src/components/BeachDetails.tsx
+++ src/components/BeachDetails.tsx
@@ -1,18 +1,31 @@
-import React from "react";
-import { useParams } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { useAppSelector } from "../app/hooks";
+import { getDate } from "../functions/getDate";
 import MapLoader from "./MapLoader";
+import { useEffect } from "react";
+import DetailedMapCard from "./DetailedMapCard";
 
 type Props = {};
 
 const BeachDetails = (props: Props) => {
     const beach = useAppSelector((state) => state.userOptions.chosenBeach);
-    const params = useParams();
-    //  console.log(params.beachId);
+    const navigator = useNavigate();
+    const today = getDate();
+
+    // Redirect to start page if no beach has been selected
+    useEffect(() => {
+        if (beach === undefined || beach.info.name === "") {
+            navigator("/");
+        }
+    });
 
     return (
-        <div>
-            <h1 className="text-3xl">{beach?.info.name}</h1>
+        <div className="beach-list font-default flex flex-col items-center mb-20">
+            <h2 className="text-xl text-dkblue mt-2">{beach?.info.name}</h2>
+            <h3 className="text-lbold text-mdblue mt-1 mb-14">
+                {today.day} {today.date}, {today.time}
+            </h3>
+            {beach && <DetailedMapCard beach={beach} />}
             <p>Tid från din position: {beach?.travelInfo?.travelTime}</p>
             <MapLoader destination={beach?.info.position} />
         </div>
---
+++ src/components/DetailedMapCard.tsx
@@ -0,0 +1,70 @@
+import { RiPinDistanceFill } from "react-icons/ri";
+import { Beach } from "../types/beachTypes";
+import { WeatherIcon } from "./WeatherIcon";
+import { BsBicycle, BsCheck, BsX } from "react-icons/bs";
+import { BiWalk } from "react-icons/bi";
+import { FaWind } from "react-icons/fa"
+import { MdAccessibleForward, MdOutlineDirectionsCar, MdOutlineDirectionsTransitFilled } from "react-icons/md";
+
+type Props = {
+    beach: Beach;
+};
+
+const DetailedMapCard = (props: Props) => {
+    return (
+        <div className="flex flex-row">
+            <div className="flex flex-col bg-dkblue p-10">
+                <div className="flex flex-col">
+                    {props.beach.weather &&
+                        <div className="self-center">
+                            <WeatherIcon symbol={props.beach.weather?.weatherSymbol} />
+                            <p className="beach-temperature text-[1.5625em] font-semibold text-teal pt-4 pb-3.5">
+                                {props.beach.weather?.temperature} °C
+                            </p>
+                        </div>
+                    }
+                    {props.beach.info && 
+                        <div className="flex flex-col justify-start">
+                            <div className="flex flex-row items-center pb-2">
+                                <RiPinDistanceFill size={20} color="#6291BC" />
+                                <p className="text-sm ml-2 text-lgblue">
+                                    {props.beach.travelInfo?.distance}
+                                </p>
+                            </div>
+                            <div className="flex flex-row items-center pb-2">
+                                {props.beach.travelInfo?.travelMode === "BICYCLING" && <BsBicycle size={22} color="#6291BC" />}
+                                {props.beach.travelInfo?.travelMode === "DRIVING" && <MdOutlineDirectionsCar size={22} color="#6291BC" />}
+                                {props.beach.travelInfo?.travelMode === "TRANSIT" && <MdOutlineDirectionsTransitFilled size={22} color="#6291BC" />}
+                                {props.beach.travelInfo?.travelMode === "WALKING" && <BiWalk size={22} color="#6291BC" />}
+                                <p className="text-sm ml-2 text-lgblue">
+                                    {props.beach.travelInfo?.travelTime}
+                                </p>
+                            </div>
+                            <div className="flex flex-row items-center pb-2">
+                                <FaWind size={16} color="#6291BC" />
+                                <p className="text-sm ml-2 text-lgblue">
+                                    {props.beach.weather?.windSpeed} m/s
+                                </p>
+                            </div>
+                            <div className="flex flex-row items-center pb-2">
+                                <MdAccessibleForward size={22} color="#6291BC" />
+                                <div>
+                                    {props.beach.info.accessibility === true && (
+                                        <BsCheck size={24} color="#E5F3F1" />
+                                    )}
+                                    {props.beach.info.accessibility === false && (
+                                        <BsX size={24} color="#E5F3F1" />
+                                    )}
+                                </div>
+                            </div>
+                        </div>
+                    }
+                </div>
+
+            </div>
+
+        </div>
+    );
+};
+
+export default DetailedMapCard;
